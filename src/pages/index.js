6eb77b09d401301f0b4c8eec07c32997000c4b9a
cdanmar <cdanmar@gmail.com>
2018-07-30 17:43:16 -0500

    added third page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,13 @@ export default () => (
     <div style={{ color: `tomato` }}>
         <h1>Hello Gatsby!</h1>
         <p>What a world.</p>
-        <img src="https://source.unsplash.com/random/400x200" alt="" />
+        <img src="https://source.unsplash.com/random/400x200" alt="Random from Unsplash" />
         <br />
         <div>
-            <Link to="/page-2/">Link</Link>
+            <Link to="/page-2/">Link 2</Link>
+        </div>
+        <div>
+            <Link to="/page-3">Link 3</Link>
         </div>
     </div>
 )
---
+++ src/pages/page-3.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default () => (
+    <div>
+        <p>This is the third page</p>
+        <Link to="/">back home</Link>
+    </div>
+)
